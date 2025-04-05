24c94986513f504a263c2aba337c3dcbb35e4697
geunseonkim <guensoen@gmail.com>
2025-04-06 10:35:09 +1200

    fix: remove duplicate setLoading(true) call#

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ function App() {
     try {
       setLoading(true);
       let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-      setLoading(true);
       let response = await fetch(url);
       let data = await response.json();
       setWeather(data);
